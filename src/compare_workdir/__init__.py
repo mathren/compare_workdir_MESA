4c5dd6110bdbcda812a20045d998bc9f3d6f2623
math <mathren90@gmail.com>
2021-11-09 12:15:25 -0500

    name fixes

--- src/compare_workdir/__init__.py
+++ src/compare_workdir/__init__.py
@@ -1,5 +1,5 @@
 __author__ = "Mathieu Renzo"
 
 from .compare_inlists import compare_inlists
-from .compare_all_work_dir_inlists import compare_all_work_dir_inlists
+from .compare_all_workdir_inlists import compare_all_workdir_inlists
 from .merge_column_lists import merge_column_lists
